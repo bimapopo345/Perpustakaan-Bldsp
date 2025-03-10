f5b2e1f262101ab3549236ef9f596f04d44898e0
bimapopo345 <bimapopo345@gmail.com>
2025-03-10 13:59:48 +0700

    Add: User Book List V3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -97,15 +97,15 @@
                 </div>
                 
                 <div class="space-y-4">
-                    <div class="flex flex-col md:flex-row gap-6">
-                        <div class="w-full md:w-1/3">
+                    <div class="flex flex-col gap-6">
+                        <div class="w-full">
                             <template x-if="selectedBook?.thumbnail_path">
                                 <img :src="selectedBook?.thumbnail_path" 
-                                     class="w-full rounded-lg shadow-md" 
+                                     class="w-full max-h-[400px] object-contain rounded-lg shadow-md mx-auto" 
                                      :alt="'Thumbnail ' + selectedBook?.judul">
                             </template>
                             <template x-if="!selectedBook?.thumbnail_path">
-                                <div class="w-full h-48 bg-gray-200 rounded-lg flex items-center justify-center">
+                                <div class="w-full h-64 bg-gray-200 rounded-lg flex items-center justify-center">
                                     <svg class="w-12 h-12 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                                               d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
