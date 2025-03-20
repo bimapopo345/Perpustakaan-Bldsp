ee4be28cb6a77ce9c58ecb9e962f47651fb7af21
bimapopo345 <bimapopo345@gmail.com>
2025-03-20 09:25:11 +0700

    Peminjaman bisa tanggal V3

--- resources/views/member/dashboard.blade.php
+++ resources/views/member/dashboard.blade.php
@@ -134,7 +134,8 @@
                             <tr>
                                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Judul Buku</th>
                                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tanggal Pinjam</th>
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tanggal Kembali</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tanggal Kembali (Rencana)</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tanggal Pengembalian</th>
                                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
                                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Aksi</th>
                             </tr>
@@ -150,6 +151,15 @@
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                         {{ $peminjaman->tanggal_kembali->format('d M Y') }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm">
+                                        @if($peminjaman->tanggal_dikembalikan)
+                                            <span class="text-gray-500">{{ $peminjaman->tanggal_dikembalikan->format('d M Y') }}</span>
+                                        @elseif($peminjaman->status === 'menunggu_konfirmasi_kembali')
+                                            <span class="text-indigo-600">Menunggu Konfirmasi</span>
+                                        @else
+                                            <span class="text-gray-400">-</span>
+                                        @endif
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full
